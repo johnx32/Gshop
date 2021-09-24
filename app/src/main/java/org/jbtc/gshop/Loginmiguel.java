5dddb9286d991b842647bb08c3a111db8dd56c82
L1307 <60495883+L1307@users.noreply.github.com>
2021-09-24 17:34:38 -0500

    loginmiguel

---
+++ app/src/main/java/org/jbtc/gshop/Loginmiguel.java
@@ -0,0 +1,14 @@
+package org.jbtc.gshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Loginmiguel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginmiguel);
+    }
+}
