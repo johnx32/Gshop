d17cd924b9ba4234577bee33419723a17a7dbc68
johnx32 <johnxt32@gmail.com>
2021-09-24 21:07:32 -0400

    - slideshow eliminado

--- app/src/main/java/org/jbtc/gshop/MainActivity.java
+++ app/src/main/java/org/jbtc/gshop/MainActivity.java
@@ -36,12 +36,19 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        createDrawerLayout();
+
+    }
+
+    private void createDrawerLayout(){
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home,
+                R.id.nav_gallery)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
@@ -62,4 +69,6 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
 }
