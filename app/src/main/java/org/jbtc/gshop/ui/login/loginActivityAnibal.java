3fc154c434d9b45485a3dc317248f20ec099da17
carnivale <anibal_ramirez_@hotmail.com>
2021-09-24 19:58:11 -0500

    Primer commit loginActivity.

---
+++ app/src/main/java/org/jbtc/gshop/ui/login/loginActivityAnibal.java
@@ -0,0 +1,16 @@
+package org.jbtc.gshop.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.jbtc.gshop.R;
+
+public class loginActivityAnibal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_anibal2);
+    }
+}
