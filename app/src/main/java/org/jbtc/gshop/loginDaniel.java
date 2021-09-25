d32d5346b3908c5c6d83ac098a6d2e05805bb5d2
Daniel <agented23@gmail.com>
2021-09-24 21:15:44 -0500

    - agregando loginDaniel

---
+++ app/src/main/java/org/jbtc/gshop/loginDaniel.java
@@ -0,0 +1,14 @@
+package org.jbtc.gshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginDaniel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_daniel);
+    }
+}
