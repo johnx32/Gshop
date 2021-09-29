ca3cfbfb4b8690b1b5150d06e224da6bca8e9af2
johnx32 <johnxt32@gmail.com>
2021-09-29 19:54:51 -0400

    - Productos agregado

---
+++ app/src/main/java/org/jbtc/gshop/ui/productos/ProductosFragment.java
@@ -0,0 +1,15 @@
+package org.jbtc.gshop.ui.productos;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ProductosFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
