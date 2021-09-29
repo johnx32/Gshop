af8cdc807e7ad031e5bda45de0b35dbd9b26fd5f
YorchM <jorgemtrabajo2021@gmail.com>
2021-09-29 11:13:56 -0500

    Productos Agregados

---
+++ app/src/main/java/org/jbtc/gshop/ui/productos/ProductosFragment.java
@@ -0,0 +1,25 @@
+package org.jbtc.gshop.ui.productos;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import org.jbtc.gshop.databinding.FragmentProductosBinding;
+
+
+public class ProductosFragment extends Fragment {
+
+    private FragmentProductosBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+                binding = FragmentProductosBinding.inflate(inflater,container,false);
+                return binding.getRoot();
+            }
+
+
+
+}
