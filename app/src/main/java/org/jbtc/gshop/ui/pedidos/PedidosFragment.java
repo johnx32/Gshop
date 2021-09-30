7cef0f900ef21a4da5c28291b3441ea30cede788
Raikonif <raikonif@gmail.com>
2021-09-29 20:24:53 -0400

    added pedidos

---
+++ app/src/main/java/org/jbtc/gshop/ui/pedidos/PedidosFragment.java
@@ -0,0 +1,25 @@
+package org.jbtc.gshop.ui.pedidos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.jbtc.gshop.R;
+import org.jbtc.gshop.databinding.FragmentPedidosBinding;
+
+public class PedidosFragment extends Fragment {
+
+    private FragmentPedidosBinding binding;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentPedidosBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+//        return inflater.inflate(R.layout.fragment_pedidos, container, false);
+    }
+}
