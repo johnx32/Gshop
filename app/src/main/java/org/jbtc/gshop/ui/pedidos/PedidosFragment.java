941faea2ae1ad2b2009797c23add8aeb6c302f96
johnx32 <johnxt32@gmail.com>
2021-09-29 21:10:50 -0400

    - clase producto creado

---
+++ app/src/main/java/org/jbtc/gshop/ui/pedidos/PedidosFragment.java
@@ -0,0 +1,21 @@
+package org.jbtc.gshop.ui.pedidos;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.jbtc.gshop.databinding.FragmentPedidosBinding;
+
+public class PedidosFragment extends Fragment {
+    private FragmentPedidosBinding binding;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentPedidosBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+}
