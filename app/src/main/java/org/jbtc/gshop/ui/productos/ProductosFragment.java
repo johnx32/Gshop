94f40aa10b17eda5d36cdfd1906b0b8677830799
carnivale <anibal_ramirez_@hotmail.com>
2021-09-29 21:27:37 -0500

    Añadir Pedidos.

--- app/src/main/java/org/jbtc/gshop/ui/productos/ProductosFragment.java
+++ app/src/main/java/org/jbtc/gshop/ui/productos/ProductosFragment.java
@@ -1,36 +1,17 @@
 package org.jbtc.gshop.ui.productos;
 
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import org.jbtc.gshop.databinding.FragmentProductosBinding;
 
 public class ProductosFragment extends Fragment {
     private FragmentProductosBinding binding;
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = FragmentProductosBinding.inflate(inflater, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentProductosBinding.inflate(inflater,container,false);
         return binding.getRoot();
-=======
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-public class ProductosFragment extends Fragment {
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
->>>>>>> login
     }
 }
