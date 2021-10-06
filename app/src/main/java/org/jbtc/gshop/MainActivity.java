fef5e4d1a767c147f461f1f4ff89eb4b0f36c7d7
carnivale <anibal_ramirez_@hotmail.com>
2021-10-06 00:31:06 -0500

    Commit Categoria DAO, Entity, VM

--- app/src/main/java/org/jbtc/gshop/MainActivity.java
+++ app/src/main/java/org/jbtc/gshop/MainActivity.java
@@ -27,7 +27,9 @@ import androidx.room.Room;
 
 import org.jbtc.gshop.databinding.ActivityMainBinding;
 import org.jbtc.gshop.db.GshopRoom;
+import org.jbtc.gshop.db.entity.Categoria;
 import org.jbtc.gshop.db.entity.Producto;
+import org.jbtc.gshop.db.viewmodel.CategoriaViewModel;
 import org.jbtc.gshop.db.viewmodel.ProductoViewModel;
 
 import java.util.List;
@@ -103,6 +105,16 @@ public class MainActivity extends AppCompatActivity {
                     public void accept(List<Producto> productos, Throwable throwable) throws Exception {
                         if(throwable==null)
                             binding.textviewprueba.setText(productos.get(0).nombre);
+                    }
+                });
+
+        CategoriaViewModel categoriaViewModel = new ViewModelProvider(this).get(CategoriaViewModel.class);
+        categoriaViewModel.getAllCategoria()
+                .subscribe(new BiConsumer<List<Categoria>, Throwable>() {
+                    @Override
+                    public void accept(List<Categoria> categorias, Throwable throwable) throws Exception {
+                        if(throwable==null)
+                            binding.textviewprueba.setText(categorias.get(0).nombre);
                     }
                 });
 
