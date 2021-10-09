6e1e4212d5f558a795e2ed8bb04e2a8f9f438a4e
johnx32 <johnxt32@gmail.com>
2021-10-08 21:13:30 -0400

    - Test firebase room, ok 2

--- app/src/main/java/org/jbtc/gshop/ui/home/HomeFragment.java
+++ app/src/main/java/org/jbtc/gshop/ui/home/HomeFragment.java
@@ -22,7 +22,7 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
- 
+
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
