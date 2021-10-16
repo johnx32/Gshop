4e0c52e8a6ee03b023404d314bfbee071cd55b68
kennyesid02 <kennyesid@gmail.com>
2021-10-16 22:17:13 +0200

    cambio de iconos por Yesid

--- app/src/main/java/org/jbtc/gshop/ui/categorias/categoria/CategoriaEditFragment.java
+++ app/src/main/java/org/jbtc/gshop/ui/categorias/categoria/CategoriaEditFragment.java
@@ -2,6 +2,8 @@ package org.jbtc.gshop.ui.categorias.categoria;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -9,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.jbtc.gshop.R;
 import org.jbtc.gshop.databinding.FragmentCategoriaEditBinding;
 
 public class CategoriaEditFragment extends Fragment {
@@ -20,5 +23,18 @@ public class CategoriaEditFragment extends Fragment {
         binding =  FragmentCategoriaEditBinding.inflate(inflater,container,false);
         getActivity().setTitle("Editar Categoria");
         return binding.getRoot();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.menu_op_basic,menu);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
